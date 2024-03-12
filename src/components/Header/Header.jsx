b2f8699fc9c63e3331a04ddaa76d603cb238b7e3
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-03-12 19:24:49 +0600

    project basice setap

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-5xl bg-red-500 font-extrabold p-5'>Knowledge-Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
